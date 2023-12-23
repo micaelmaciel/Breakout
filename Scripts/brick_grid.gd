83ba18f2f631edb92687d38f205d1458ace69d23
Michael <ragnarokz440@gmail.com>
2023-12-23 00:50:18 -0300

    Add color variety to grid of bricks

--- Scripts/brick_grid.gd
+++ Scripts/brick_grid.gd
@@ -5,6 +5,7 @@ extends Node2D
 @export var hSeparation: int = 24
 @export var vSeparation: int = 25
 
+#TODO: use a better way to get the sizes
 var brickVSize: int = 18
 var brickHSize: int = 87
 
@@ -14,11 +15,11 @@ func _ready() -> void:
 	var currentX: float = position.x
 	var currentY: float = position.y
 
-	#FIXBUG: the order is not correct and apparently neither the separation, which should take into account the size of the bricks
 	for column: int in range(columns):
 		for row: int in range(rows):
 			var instanceBrick: StaticBody2D = packedBrick.instantiate()
 			instanceBrick.global_position = Vector2(currentX, currentY)
+			instanceBrick.modulate = Color(randf(), randf(), randf())
 			add_child(instanceBrick)
 			currentY += vSeparation + brickVSize
 		currentX += hSeparation + brickHSize
