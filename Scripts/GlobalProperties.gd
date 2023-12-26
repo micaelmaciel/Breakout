52359b87889f86fc83729432ca69cd603f1f1472
Michael <ragnarokz440@gmail.com>
2023-12-26 18:40:51 -0300

    Added score and fixed some collisions

--- Scripts/GlobalProperties.gd
+++ Scripts/GlobalProperties.gd
@@ -2,5 +2,8 @@ extends Node
 
 signal out_bounds
 
+var score: int = 0
+var highestScore: int = 0
+
 func emit_out_bounds() -> void:
 	out_bounds.emit()
--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 @export var initialSpeed: int = 100
+var score: int = GlobalProperties.score
+var highestScore: int = GlobalProperties.highestScore
 
-func bounce_ball(collision: KinematicCollision2D) -> void:
-	velocity = velocity.bounce(collision.get_normal())
 
 #region ball starting
 func initialize_ball() -> void:
@@ -14,8 +14,14 @@ func initialize_ball() -> void:
 	velocity *= initialSpeed
 
 func reinitialize_ball() -> void:
-	pass
+	set_collision_mask_value(1, true)
+	position = Vector2(463, 363)
+	initialize_ball()
 #endregion
+
+#region ball collision
+func bounce_ball(collision: KinematicCollision2D) -> void:
+	velocity = velocity.bounce(collision.get_normal())
 
 func react_collision(collision: KinematicCollision2D) -> void:
 	var collider: Node = collision.get_collider()
@@ -23,12 +29,22 @@ func react_collision(collision: KinematicCollision2D) -> void:
 	if (collider.is_in_group("brick")):
 		collider.queue_free.call_deferred()
 		velocity.y *= 1.2
+		GlobalProperties.score += 1
+		bounce_ball(collision)
 	elif (collider is Player):
-		var normalizedDifference: float = (position - collider.position).normalized().x
-		var speed: float = velocity.length()
-		velocity.x = normalizedDifference * speed
-		velocity = velocity.normalized() * speed
-		
+		var normal_x: float = collision.get_normal().x
+		if (abs(abs(normal_x) - 1) < 0.0001):
+			set_collision_mask_value(1, false)
+		else:
+			var normalizedDifference: float = (position - collider.position).normalized().x
+			var speed: float = velocity.length()
+			velocity.x = normalizedDifference * speed
+			velocity = velocity.normalized() * speed
+			bounce_ball(collision)
+	else:
+		bounce_ball(collision)
+#endregion
+
 func _ready() -> void:
 	initialize_ball()
 
@@ -36,8 +52,9 @@ func _physics_process(delta: float) -> void:
 	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
 	if (collision):
 		react_collision(collision)
-		bounce_ball(collision)
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	GlobalProperties.highestScore = max(GlobalProperties.highestScore, GlobalProperties.score)
+	GlobalProperties.score = 0
 	GlobalProperties.emit_out_bounds()
 	reinitialize_ball()
